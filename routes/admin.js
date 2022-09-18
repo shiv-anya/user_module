e2080be165b7f32782b61601cafbf09e08bc68a6
shiv-anya <sg90883@gmail.com>
2022-09-19 01:09:06 +0530

    added routes to admin for CRUD ops for roles and users

---
+++ routes/admin.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+const express = require("express");
+
+const adminController = require("../controllers/admin");
+
+const router = express.Router();
+
+router.get("/users", adminController.readUsers);
+
+router.post("/add-user", adminController.createUser);
+
+router.patch("/edit-user", adminController.updateUser);
+
+router.delete("/delete-user/:userId", adminController.deleteUser);
+
+router.get("/roles", adminController.readRoles);
+
+router.post("/add-role", adminController.createRole);
+
+// router.patch("/edit-role", adminController.updateRole);
+
+router.delete("/delete-role/:roleId", adminController.deleteRole);
+
+module.exports = router;
