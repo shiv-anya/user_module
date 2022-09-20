4ac0b79eadfb16c3706727d879faccdf52719003
shiv-anya <sg90883@gmail.com>
2022-09-20 21:18:48 +0530

    changed role schema

--- models/role.js
+++ models/role.js
@@ -19,39 +19,4 @@ const roleSchema = new Schema({
   ],
 });
 
-// userSchema.methods.addToCart = function (product) {
-//   const cartItemIndex = this.cart.items.findIndex((p) => {
-//     return p.productId.toString() === product._id.toString();
-//   });
-//   let newQuantity = 1;
-//   const updatedCartItems = [...this.cart.items];
-//   if (cartItemIndex >= 0) {
-//     newQuantity = this.cart.items[cartItemIndex].quantity + 1;
-//     updatedCartItems[cartItemIndex].quantity = newQuantity;
-//   } else {
-//     updatedCartItems.push({
-//       productId: product._id,
-//       quantity: newQuantity,
-//     });
-//   }
-//   const updatedCart = {
-//     items: updatedCartItems,
-//   };
-//   this.cart = updatedCart;
-//   return this.save();
-// };
-
-// userSchema.methods.deleteCartItems = function (prodId) {
-//   const updatedItems = this.cart.items.filter(
-//     (p) => p.productId.toString() !== prodId.toString()
-//   );
-//   this.cart = updatedItems;
-//   return this.save();
-// };
-
-// userSchema.methods.clearCart = function () {
-//   this.cart = { items: [] };
-//   return this.save();
-// };
-
 module.exports = mongoose.model("Role", roleSchema);
