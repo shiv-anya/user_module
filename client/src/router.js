da66f148fc0368b67e70a579a51140da4de610b8
shiv-anya <sg90883@gmail.com>
2022-09-22 17:59:10 +0530

    added path to edit user and del

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Dashboard from "./components/Dashboard/Dashboard";
 import RolesPage from "./pages/RolesPage";
+import UsersList from "./components/users/UsersList";
+import UserForm from "./components/users/UserForm";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,11 +14,18 @@ const router = createRouter({
       path: "/admin/roles",
       component: RolesPage,
     },
-    // {
-    //   path: '/users',
-    //   components: {
-
-    //   },
+    {
+      path: "/admin/users",
+      component: UsersList,
+    },
+    {
+      path: "/admin/add-user",
+      component: UserForm,
+    },
+    {
+      path: "/admin/delete-user/:userId",
+      component: UsersList,
+    },
     // { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: "active",
