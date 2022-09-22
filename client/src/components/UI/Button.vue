121992d87591d99025d6f318c3bedf4b4e269914
shiv-anya <sg90883@gmail.com>
2022-09-22 17:50:49 +0530

    added customisable Button to ui

---
+++ client/src/components/UI/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :style="`background-color: ${color}`" type="type">
+    {{ name }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["name", "color", "type"],
+};
+</script>
+
+<style scoped>
+button {
+  border: none;
+  border-radius: 10px;
+  padding: 10px 20px;
+  text-align: center;
+  cursor: pointer;
+  width: 100px;
+  margin-right: 12px;
+  color: #fff;
+}
+</style>
