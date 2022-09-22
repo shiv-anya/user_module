effcc078f2fa3910dc75df886227d19a49ddabf7
shiv-anya <sg90883@gmail.com>
2022-09-23 01:06:22 +0530

    new file added to showcase members

--- client/src/components/roles/RolesMembers.vue
+++ client/src/components/roles/RolesMembers.vue
@@ -1,79 +1,67 @@
 <template>
-    <section>
-      <h2>{{ teamName }}</h2>
-      <ul>
-        <user-item
-          v-for="member in members"
-          :key="member.id"
-          :name="member.fullName"
-          :role="member.role"
-        ></user-item>
-      </ul>
-    </section>
-  </template>
+  <section>
+    <h2>{{ teamName }}</h2>
+    <ul>
+      <user-item
+        v-for="member in members"
+        :key="member.id"
+        :name="member.fullName"
+        :role="member.role"
+      ></user-item>
+    </ul>
+  </section>
+</template>
   
   <script>
-  import UserItem from '../users/UserItem.vue';
-  
-  export default {
-    props: ['name', 'member-count'],
-    components: {
-      UserItem,
+import UserItem from "../users/UserItem.vue";
+import axios from "axios";
+export default {
+  props: ["name", "member-count"],
+  components: {
+    UserItem,
+  },
+  data() {
+    return {
+      teamName: "",
+      members: [],
+    };
+  },
+  methods: {},
+  created() {
+    // this.$route.path // /teams/t1
+    this.loadTeamMembers(this.teamId);
+    console.log(this.$route.query);
+  },
+  beforeRouteUpdate(to, from, next) {
+    console.log("TeamMembers Cmp beforeRouteUpdate");
+    console.log(to, from);
+    // this.loadTeamMembers(to.params.teamId);
+    next();
+  },
+  watch: {
+    teamId(newId) {
+      this.loadTeamMembers(newId);
     },
-    data() {
-      return {
-        teamName: '',
-        members: [],
-      };
-    },
-    methods: {
-      loadTeamMembers(teamId) {
-        const selectedTeam = this.teams.find((team) => team.id === teamId);
-        const members = selectedTeam.members;
-        const selectedMembers = [];
-        for (const member of members) {
-          const selectedUser = this.users.find((user) => user.id === member);
-          selectedMembers.push(selectedUser);
-        }
-        this.members = selectedMembers;
-        this.teamName = selectedTeam.name;
-      },
-    },
-    created() {
-      // this.$route.path // /teams/t1
-      this.loadTeamMembers(this.teamId);
-      console.log(this.$route.query);
-    },
-    beforeRouteUpdate(to, from, next) {
-      console.log('TeamMembers Cmp beforeRouteUpdate');
-      console.log(to, from);
-      // this.loadTeamMembers(to.params.teamId);
-      next();
-    },
-    watch: {
-      teamId(newId) {
-        this.loadTeamMembers(newId);
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
   
   <style scoped>
-  section {
-    margin: 2rem auto;
-    max-width: 40rem;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    padding: 1rem;
-    border-radius: 12px;
-  }
-  
-  h2 {
-    margin: 0.5rem 0;
-  }
-  
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-  </style>
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  border-radius: 12px;
+}
+
+h2 {
+  margin: 0.5rem 0;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
