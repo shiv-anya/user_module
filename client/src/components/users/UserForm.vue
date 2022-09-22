bcdcfc029a8679dbde8dd8a024dc33cab807fbf5
shiv-anya <sg90883@gmail.com>
2022-09-22 17:51:50 +0530

    added addUser feature with delete user

---
+++ client/src/components/users/UserForm.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="addUser" method="POST" action="/admin/add-user">
+      <div class="outer">
+        <div class="inner">
+          <label for="firstname">First name</label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            v-model.trim="firstName"
+          />
+        </div>
+        <div class="inner">
+          <label for="lastname">Last name</label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            v-model.trim="lastName"
+          />
+        </div>
+      </div>
+      <div class="outer">
+        <div class="inner">
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="inner">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            v-model.trim="password"
+          />
+        </div>
+      </div>
+      <div class="inner">
+        <label for="roles">Roles</label>
+        <select name="roles" id="roles" v-model="role">
+          <option value="Accountant">Accountant</option>
+          <option value="Finance">Finance</option>
+          <option value="Engineer">Engineer</option>
+          <option value="Analyst">Analyst</option>
+        </select>
+      </div>
+      <div class="inner">
+        <label for="accesstype">Access Type</label>
+        <select name="accesstype" id="accesstype" v-model.trim="accessType">
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
+      </div>
+      <Button name="Submit" color="rgb(0,195,255)" type="submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+import Button from "../UI/Button.vue";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      role: "",
+      accessType: "",
+    };
+  },
+  methods: {
+    addUser() {
+      axios
+        .post("http://localhost:4000/admin/add-user", {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          password: this.password,
+          role: this.role,
+          accessType: this.accessType,
+        })
+        .then((data) => {
+          this.firstName = "";
+          this.lastName = "";
+          this.email = "";
+          this.password = "";
+          this.role = "";
+          this.accessType = "";
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 1000px;
+  margin: 0 auto;
+  padding: 2rem 0;
+  display: flex;
+  flex-direction: column;
+  font-size: 1.2rem;
+}
+.outer {
+  width: 900px;
+  display: flex;
+  justify-content: space-between;
+}
+.inner {
+  display: flex;
+  flex-direction: column;
+}
+.inner input {
+  width: 440px;
+  padding: 0.3rem;
+  margin: 10px 0 10px 0;
+}
+.inner select {
+  width: 440px;
+  padding: 0.3rem;
+  margin: 10px 0;
+}
+select:focus,
+input:focus {
+  outline: none;
+  border: 3px solid rgb(0, 195, 255);
+  border-radius: 5px;
+}
+</style>
---
+++ client/src/components/users/UsersList.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="container">
+    <router-link to="/admin/add-user" class="btn btn-primary"
+      >Add user</router-link
+    >
+    <ul>
+      <li v-for="user in users" :key="user._id">
+        <div class="card">
+          <p>{{ user.firstName + " " + user.lastName }}</p>
+          <p>{{ user.email }}</p>
+          <Button :name="user.role" color="rgb(0, 195, 255)" />
+          <div class="actions">
+            <form
+              @submit="deleteUser(user._id)"
+              method="DELETE"
+              :action="`/admin/delete-user/${user._id}`"
+            >
+              <Button name="Delete" color="#F11E1B" type="submit" />
+              <router-link to="/admin/edit-user" class="btn btn-success"
+                >Edit</router-link
+              >
+            </form>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Button from "../UI/Button.vue";
+
+export default {
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    getResults() {
+      fetch("http://localhost:4000/admin/users")
+        .then((res) => res.json())
+        .then((data) => {
+          this.users = [...data.users];
+        })
+        .catch((err) => console.log(err));
+    },
+    deleteUser(userId) {
+      fetch(`http://localhost:4000/admin/delete-user/${userId}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    },
+  },
+  mounted() {
+    this.getResults();
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 1000px;
+  margin: 0 auto;
+  padding: 2rem 0;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+}
+.card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  width: 1000px;
+  font-size: 1rem;
+  background-color: #f4f4f4;
+  padding: 1.5rem;
+  border-radius: 10px;
+  margin: 10px 0px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
+    rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+}
+li {
+  list-style: none;
+}
+
+.btn {
+  border-radius: 10px;
+  cursor: pointer;
+  margin-right: 12px;
+  color: #fff;
+  padding: 10px 40px;
+  font-size: 1rem;
+  text-decoration: none;
+  /* width: 200px; */
+}
+.btn-primary {
+  background-color: rgb(0, 195, 255);
+}
+.btn-success {
+  background-color: #81f11b;
+}
+</style>
