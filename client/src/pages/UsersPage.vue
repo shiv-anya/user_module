a54cdecb1e67e5d035d57ef67a02a9ce441c80cb
shiv-anya <sg90883@gmail.com>
2022-09-22 17:54:53 +0530

    fetching user and displaying them

---
+++ client/src/pages/UsersPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <UsersList />
+</template>
+
+<script>
+import UsersList from "../components/users/UsersList.vue";
+
+export default {
+  components: {
+    UsersList,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 1000px;
+  margin: 0 auto;
+  padding: 2rem 0;
+}
+</style>
