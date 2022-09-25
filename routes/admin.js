02d8eb0dc14a98600222993be5d5311b20d0cc9c
shiv-anya <sg90883@gmail.com>
2022-09-25 17:14:17 +0530

    added routes to del and add user to different roles

--- routes/admin.js
+++ routes/admin.js
@@ -6,21 +6,29 @@ const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
+//Users Routes if user is user only
 router.get("/users", adminController.readUsers);
 
+router.get("/users/:userId", adminController.getSingleUser);
+
+//Users Routes if user is admin
 router.post("/add-user", adminController.createUser);
 
 router.patch("/edit-user/:userId", adminController.updateUser);
 
 router.delete("/delete-user/:userId", adminController.deleteUser);
 
-router.get("/users/:userId", adminController.getSingleUser);
-
+//Roles routes if user is user only
 router.get("/roles", adminController.readRoles);
 
+router.get("/roles/:roleId", adminController.getSingleRole);
+
+//Roles routes for admin
 router.post("/add-role", adminController.createRole);
 
-router.patch("/edit-role/:roleId", adminController.updateRole);
+router.delete("/roles/:roleId/:userId", adminController.deleteUserFromRole);
+
+router.patch("/roles/:roleId", adminController.addUserToRole);
 
 router.delete("/delete-role/:roleId", adminController.deleteRole);
 
