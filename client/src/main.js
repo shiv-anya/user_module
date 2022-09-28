9c95680be16db802bff2b5b30cb7f678d61886ed
shiv-anya <sg90883@gmail.com>
2022-09-28 23:02:21 +0530

    store being configured

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,12 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-
-// const router = require('./router');
-
-// createApp(App).mount('#app')
 import { createApp } from "vue";
 
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 
 const app = createApp(App);
 
+app.use(store);
 app.use(router);
 
 app.mount("#app");
