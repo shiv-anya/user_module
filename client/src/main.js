22daee29169a3961f967a66ad465c3757e5d45d9
shiv-anya <sg90883@gmail.com>
2022-10-11 00:10:49 +0530

    adding base url

--- client/src/main.js
+++ client/src/main.js
@@ -6,11 +6,6 @@ import store from "./store";
 
 const app = createApp(App);
 
-if (process.env.NODE_ENV === "production") {
-  process.env.APP_BASE_URL = "https://user-module22.herokuapp.com";
-} else {
-  process.env.APP_BASE_URL = "http://localhost:3000";
-}
 app.use(store);
 app.use(router);
 
